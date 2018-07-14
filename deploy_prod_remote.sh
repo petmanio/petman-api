ae995567b4b6e4280d04634325644fd16de4ed71
Andranik Antonyan <andantonyan@gmail.com>
2018-07-14 22:00:40 +0400

    typo

--- deploy_prod_remote.sh
+++ deploy_prod_remote.sh
@@ -8,4 +8,3 @@ rsync -a --delete --progress config/ andranik@petman.io:/opt/petman-api/config
 rsync -a --delete --progress dist/ andranik@petman.io:/opt/petman-api/dist
 rsync -a --delete --progress package.json andranik@petman.io:/opt/petman-api/package.json
 rsync -a --delete --progress package-lock.json andranik@petman.io:/opt/petman-api/package-lock.json
-
