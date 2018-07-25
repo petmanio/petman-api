1cbc45eea1108e34f826114a222759991d517f07
Andranik Antonyan <andantonyan@gmail.com>
2018-07-25 21:17:09 +0400

    chore(depoloy): some polishing

--- deploy_prod_remote.sh
+++ deploy_prod_remote.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf ./dist/
 npm run build:prod
 
-rsync -a --delete --progress config/ andranik@petman.io:/opt/petman-api/config
-rsync -a --delete --progress dist/ andranik@petman.io:/opt/petman-api/dist
-rsync -a --delete --progress package.json andranik@petman.io:/opt/petman-api/package.json
-rsync -a --delete --progress package-lock.json andranik@petman.io:/opt/petman-api/package-lock.json
+# rsync -a --delete --progress config/ production@petman.io:/opt/petman-api/config
+rsync -a --delete --progress dist/ production@petman.io:/opt/petman-api/dist
+rsync -a --delete --progress package.json production@petman.io:/opt/petman-api/package.json
+# rsync -a --delete --progress package-lock.json production@petman.io:/opt/petman-api/package-lock.json
